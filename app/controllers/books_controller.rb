1955f47fedf4bb7f87bebcebacf2d76717369c08
EC2 Default User <ec2-user@ip-172-31-27-223.ec2.internal>
2022-02-16 12:15:48 +0000

    [Finish]thema2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,19 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new #2022/2/16(debug)
+    @user = User.find(@book.user_id) #2022/2/16(debug)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id #2022/2/16(debug)
+
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -20,6 +25,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    # 2022/2/16(debug)
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
@@ -31,15 +42,17 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  # def delete
+  def destroy #2022/2/16(debug)
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    # params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body) #2022/2/16(debug)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  # before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update, :edit] #2022/2/16(debug)
 
   def show
     @user = User.find(params[:id])
@@ -13,13 +14,16 @@ class UsersController < ApplicationController
   end #2022/2/15(debug)
 
   def edit
+    @user = User.find(params[:id]) #2022/2/16(debug)
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      # redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully." #2022/2/16(debug)
     else
-      render "show"
+      # render "show"
+      render "edit" #2022/2/16(debug)
     end
   end
 
